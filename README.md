dbe17c2cf55a86f902cbb1a7515ea13d235801ee
renzivanyapenguio@gmail.com <renzivanyapenguio@gmail.com>
2020-10-18 15:22:57 +0800

    lesson 2: compiling

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Finance Logger App Using TypeScript
+
+### Lesson 2: Compiling TypeScript
+
+```
+# Compile
+$ tsc <file.ts> <file.js>
+
+# Watch and compile
+$ tsc <file.ts> <file.js> -w
+```
---
+++ sandbox.ts
@@ -0,0 +1,11 @@
+const character = 'renz'
+
+console.log(character)
+
+const inputs = document.querySelectorAll('input')
+
+console.log('inputs: ', inputs)
+
+inputs.forEach(input => {
+    console.log(input)
+})
