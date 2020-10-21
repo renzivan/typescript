30cee3fe91bb19b0716d71f6b2c5513f9f01280d
renzivan <renzivanyapenguio@gmail.com>
2020-10-21 14:21:05 +0800

    objects and arrays

--- sandbox.js
+++ sandbox.js
@@ -1,44 +1,33 @@
 // arrays
-let names = ['renz', 'ivan', 'zen z']
-
+var names = ['renz', 'ivan', 'zen z'];
 // can't push to different type
 // names = ''
-
-names.push('test')
+names.push('test');
 // can't push to different type
 // names.push(3)
 // names[0] = 3
-
-let numbers = [1, 2, 3, 4]
-
-numbers.push(25)
+var numbers = [1, 2, 3, 4];
+numbers.push(25);
 // can't push to different type
 // numbers.push('test')
-
-let mixed = ['ken', 4, 'ryu', 2, 3]
-
-mixed.push('test')
-mixed.push(23)
-mixed[0] = 5
-
+var mixed = ['ken', 4, 'ryu', 2, 3];
+mixed.push('test');
+mixed.push(23);
+mixed[0] = 5;
 // objects
-let ninja = {
+var ninja = {
     name: 'renz',
     belt: 'black',
     age: 30
-}
-
-ninja.age = 40
-ninja.name = 'ivan'
-
+};
+ninja.age = 40;
+ninja.name = 'ivan';
 // can't set to different type
 // ninja.age = 'test'
-
 // can't add property
 // ninja.skills = ['fighting', 'sneaking']
-
 ninja = {
     name: 'test',
     belt: 'orange',
     age: 10
-}
+};
