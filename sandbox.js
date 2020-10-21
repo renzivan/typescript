0188359101a65655d3668f3a2f36a0d7aaf192ae
renzivan <renzivanyapenguio@gmail.com>
2020-10-21 14:09:33 +0800

    type basics

---
+++ sandbox.js
@@ -0,0 +1,13 @@
+var character = 'mario';
+var age = 30;
+var isBlackBelt = false;
+// character = 90
+character = 'renz';
+// age = 'renz'
+age = 0;
+// isBlackBelt = 'yes'
+isBlackBelt = true;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(5.5));
---
+++ sandbox.ts
@@ -0,0 +1,18 @@
+let character = 'mario'
+let age = 30
+let isBlackBelt = false
+
+// character = 90
+character = 'renz'
+
+// age = 'renz'
+age = 0
+
+// isBlackBelt = 'yes'
+isBlackBelt = true
+
+const circ = (diameter: number) => {
+    return diameter * Math.PI
+}
+
+console.log(circ(5.5))
