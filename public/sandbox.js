b592dded4e012d7c251e4cbea25293aaf056c646
renzivan <renzivanyapenguio@gmail.com>
2020-10-22 14:52:15 +0800

    function basics

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,16 @@
 "use strict";
-console.log('sandboxaa test');
+var greet;
+greet = function () {
+    console.log('Hello, world');
+};
+// set optional = c?: number | string
+var add = function (a, b, c) {
+    if (c === void 0) { c = ''; }
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 2, 52);
+var minus = function (a, b) {
+    return a + b;
+};
+var result = minus(10, 7);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,20 @@
-console.log('sandboxaa test')
+let greet: Function
+
+greet = () => {
+  console.log('Hello, world')
+}
+
+// set optional = c?: number | string
+const add = (a: number, b: number, c: number | string = ''): void => {
+  console.log(a + b)
+  console.log(c)
+}
+
+add(5, 2, 52)
+
+const minus = (a: number, b: number): number => {
+  return a + b
+}
+
+let result = minus(10, 7)
+
